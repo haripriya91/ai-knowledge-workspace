3ac73ebd584ba32b8770017b0ff024a2a9bf9ea5
haripriya91 <geetpriya.k@gmail.com>
2026-02-02 23:56:06 +0100

    profilecard code chages

--- frontend/ai-frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/ai-frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../features/auth/auth.service';
+
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  
+  private auth = inject(AuthService);
+  constructor(private router: Router) {}
 
+  goHome() {
+    this.auth.isLoggedIn()
+      ? this.router.navigate(['/dashboard'])
+      : this.router.navigate(['/']);
+  }
 }
