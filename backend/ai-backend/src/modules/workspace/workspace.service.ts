242c33e48c13a3777e6f16fb6ccdbcd536989549
haripriya91 <geetpriya.k@gmail.com>
2026-03-21 23:54:40 +0100

    updated workspaceschema and started workspace backend implementation

--- backend/ai-backend/src/modules/workspace/workspace.service.ts
+++ backend/ai-backend/src/modules/workspace/workspace.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class WorkspaceService {}
+export class WorkspaceService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(userId: string, name: string) {
+    const workspace = await this.prisma.workspace.create({
+      data: {
+        name,
+        isPublic: false,
+        workspaceUsers: {
+          create: {
+            userId,
+            role: 'OWNER',
+          },
+        },
+      },
+    });
+
+    return workspace;
+  }
+
+  async getUserWorkspaces(userId: string) {
+    return this.prisma.workspace.findMany({
+      where: {
+        workspaceUsers: {
+          some: {
+            userId,
+          },
+        },
+      },
+    });
+  }
+
+  async joinWorkspace(userId: string, workspaceId: string) {
+    return this.prisma.workspaceUser.create({
+      data: {
+        userId,
+        workspaceId,
+        role: 'MEMBER',
+      },
+    });
+  }
+}
